e279e245e880e62a36387260180ca733132ff404
aaashwani1 <ash.19190@gmail.com>
2025-07-10 23:25:56 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "resource" "name" {
+  
+}
+
+git init
+git add .
+git commit -m "Initial commit"
+git remote add origin https://github.com/user/repo.git
+git branch -M main
+git push -u origin main
